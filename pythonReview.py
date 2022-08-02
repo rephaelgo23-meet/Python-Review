f9c1b338b69a565482b487149ab496c29d8e68d7
rephaelgo23-meet <109904475+rephaelgo23-meet@users.noreply.github.com>
2022-08-02 15:58:40 +0300

    s

--- pythonReview.py
+++ pythonReview.py
@@ -1,5 +1,5 @@
-def create_youtube_video(title, description):
-	youtubedict = { "title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}}
+def create_youtube_video(title, description, word1, word2, word3, word4, word5):
+	youtubedict = { "title": title, "description": description, "likes": 0, "dislikes": 0, "comments": {}, "hashtag": [word1, word2, word3, word4, word5]}
 	return youtubedict
 
 def like(youtube_video):
@@ -14,6 +14,9 @@ def dislikes(youtube_video):
 
 def add_comment(youtube_video, username, comment_text):
 	youtube_video["comments"][username] = comment_text
+
+def similarity_to_video(dictionary1, dictionary2):
+		
 
 dictionary = create_youtube_video("hello", "this video is about hello")
 like(dictionary)
